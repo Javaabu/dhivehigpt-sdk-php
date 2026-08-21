0d32ae2b767618d0b385434b30c8eaab909a0fb6
Arushad Ahmed <dash-8x@hotmail.com>
2026-08-21 19:41:12 +0500

    Initial commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Javaabu\{YourPackage}\Tests\Feature;
+
+use PHPUnit\Framework\Attributes\Test;
+use Javaabu\{YourPackage}\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class ExampleTest extends TestCase
+{
+    #[Test]
+    public function it_can_test_the_feature_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Javaabu\{YourPackage}\Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Test;
+use Javaabu\{YourPackage}\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class ExampleTest extends TestCase
+{
+    #[Test]
+    public function it_can_test_it_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
